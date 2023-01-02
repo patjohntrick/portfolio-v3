c2390123c92610009cb89766c81c7fbe9ffc91b4
John Patrick Resurreccion <patrickjohn.resurreccion18@gmail.com>
2023-01-02 10:09:40 +0800

    fix section title

--- components/StyledSectionTitle.tsx
+++ components/StyledSectionTitle.tsx
@@ -14,7 +14,7 @@ export const StyledSectionTitle = ({
     <div className="text-center mb-12">
       <StyledTitle
         text={title}
-        className="text-2xl font-semibold text-[#333333]"
+        className="text-2xl font-semibold text-[#333333] lg:text-4xl lg:mb-2"
       />
       <StyledSubTitle text={subTitle} className="text-black/60 text-base" />
     </div>
